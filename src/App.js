c7efd2c4df54c4aafd4e31e5b73e6d0de857210e
kaiwang22 <kaiwang1@seas.upenn.edu>
2022-10-23 13:07:50 -0400

    newest vers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 
 import './App.css';
+import Header from './components/header.js'
+import Interests from './components/interests.js'
+import Resume from '.components/resume.js'
+import subHeader from '.components/subheader.js'
 
 function App() {
   return (
-    <div className="App">
-      <h1>hi</h1>
+    <div id = "page-container">
+      <div class="page-body">
+        <Header name = "Kai Wang"> </Header>
+        <subHeader subname = "26"> </subHeader>
+        <p>hi! my name is kai! I'm currently a freshman at the university of pennsylvania studying systems science and engineering (and currently thinking about a computer science minor)! 
+                        here are some of my interests:</p>
+        <Interests interest = "entrepreneurship"> </Interests>
+        <Interests interest = "data science"> </Interests>
+        <Interests interest = "marine biology (sharks!)"> </Interests>
+        <Interests interest = "decision science"> </Interests>
+        <Interests interest = "international jazz!"> </Interests>
+        <Interests interest = "jewelry shopping"> </Interests>
+        <p>things not on my resume...:</p>
+        <Resume resume = "beat my peanut + almond allergy of 18 years"> </Resume>
+        <Resume resume = "can slightly wiggle my ears"> </Resume>
+        <Resume resume = "I have a lightning bolt scar on my right hand"> </Resume>
+        <Resume resume = "I've only lost money in the stock market"> </Resume>
+      </div>
+
     </div>
   );
 }
